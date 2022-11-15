cf8468e1a6e92704e187314dfb489c31f97eac91
Ajarek <ajarek@poczta.onet.pl>
2022-11-16 00:50:50 +0100

    added style

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -4,7 +4,7 @@ import Keyboard from './Keyboard/Keyboard'
 export default function Board() {
   return (
     <div className='wordle'>
-      <h3>WORDLE PL</h3>
+      <h3 onClick={()=>location.reload()}><span className='wordle-letter'>W</span><span className='wordle-letter'>O</span><span className='wordle-letter'>R</span><span className='wordle-letter'>D</span><span className='wordle-letter'>L</span><span className='wordle-letter'>E</span>    <span className='wordle-letter pl'>P</span><span className='wordle-letter pl'>L</span>       </h3>
       <Grid />
       <Keyboard />
     </div>
